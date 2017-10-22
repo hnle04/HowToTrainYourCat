0467d02b8ed36a2fa883ee94e627e7d62821c407
jchen790 <jchen790@gmail.com>
2017-10-22 05:58:43 -0400

    added action logic

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
+/**
+ * 0 - walk --> sit
+ * 1 - running 
+ * 2 - running 
+ * 3 - meowing
+ */
+var currCatState = 0;
+var newCatState = 0;
+
+function changeCatState(newCS)
+{
+  if (newCS !== 0 && newCS !== 1 && newCS !== 2)
+  {
+    newCatState = Math.floor(Math.random() * 3);
+  }
+
+  setTimeout(function () {
+      if (currCatState === newCatState)
+      {
+        return;
+      }
+
+      if (currCatState === 0)
+      {
+        $('#tuna').removeClass('tuna-walk-sit');
+      }
+      else if (currCatState === 1)
+      {
+        $('#tuna').removeClass('tuna-running');
+      }
+      else if (currCatState === 2)
+      {
+        $('#tuna').removeClass('tuna-walk');
+      }
+
+      if (newCatState === 0)
+      {
+        $('#tuna').addClass('tuna-walk-sit');
+      }
+      else if (newCatState === 1)
+      {
+        $('#tuna').addClass('tuna-running');
+      }
+      else if (newCatState === 2)
+      {
+        $('#tuna').addClass('tuna-walk');
+      }
+      currCatState = newCatState;
+      return;
+  }, 500);
+
+  
+  return;
+}
+
 $("#info").click(function() {
   // display information about the app and helping rescue animals
   console.log("I wanna see some info!");
 });
-
-$("#settings").click(function() {
-  // display settings modal
-  console.log("choices~");
-})
 
 $("#sit").click(function() {
   // cat MAY sit
@@ -16,10 +66,13 @@ $("#sit").click(function() {
   if (prob < 25)
   {
     console.log("sit");
+    newCatState = 0;
+    changeCatState(newCatState);
   }
-
-  $('#tuna').removeClass("tuna-walk-sit");
-  $('#tuna').addClass("tuna-running");
+  else
+  {
+    changeCatState(-1);
+  }
 })
 
 $("#meow").click(function() {
@@ -30,6 +83,8 @@ $("#meow").click(function() {
   if (prob < 30)
   {
     console.log("speak");
+    //play audio
+    changeCatState(-1);
   }
 })
 
@@ -41,6 +96,12 @@ $("#stay").click(function() {
   if (prob < 20)
   {
     console.log("stay");
+    newCatState = 1;
+    changeCatState(newCatState);
+  }
+  else
+  {
+    changeCatState(-1);
   }
 })
 
@@ -52,6 +113,12 @@ $("#stop").click(function() {
   if (prob < 15)
   {
     console.log("stop");
+    newCatState = 2;
+    changeCatState(newCatState);
+  }
+  else
+  {
+    changeCatState(-1);
   }
 })
 
