b268c2a86c3fd14437550485c2dad62f56e77f49
jchen790 <jchen790@gmail.com>
2017-10-22 01:33:58 -0400

    added probabilities into actions

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-$("info").click(function() {
+$("button.buttonA.button-info").click(function() {
   // display information about the app and helping rescue animals
 });
 
-$("settings").click(function() {
+$("button.buttonA.button-settings").click(function() {
   // display settings modal
+})
+
+$("button.button.button1").click(function() {
+  // cat MAY sit
+  let prob = Math.floor(Math.random() * 101);
+  console.log("Probablity = " + prob);
+
+  if (prob < 25)
+  {
+    console.log("sit");
+  }
+})
+
+$("button.button.button2").click(function() {
+  // cat MAY speak
+  let prob = Math.floor(Math.random() * 101);
+  console.log("Probablity = " + prob);
+  
+  if (prob < 30)
+  {
+    console.log("speak");
+  }
+})
+
+$("button.button.button3").click(function() {
+  // cat MAY stay
+  let prob = Math.floor(Math.random() * 101);
+  console.log("Probablity = " + prob);
+  
+  if (prob < 20)
+  {
+    console.log("stay");
+  }
+})
+
+$("button.button.button4").click(function() {
+  // cat MAY stop
+  let prob = Math.floor(Math.random() * 101);
+  console.log("Probablity = " + prob);
+  
+  if (prob < 15)
+  {
+    console.log("stop");
+  }
 })
